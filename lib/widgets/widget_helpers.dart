2f5b1d0271e16171eb7774b4c24203284911d34f
Rajkishore Sahu <coolfire890@gmail.com>
2021-11-22 11:35:10 +0530

    [INFO] Common icon button added

--- lib/widgets/widget_helpers.dart
+++ lib/widgets/widget_helpers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class WidgetHelper {
   static Widget commonButton(
@@ -9,14 +10,48 @@ class WidgetHelper {
         width: buttonWidth,
         child: Container(
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
+              borderRadius: BorderRadius.circular(30.0),
               gradient: const LinearGradient(
                   begin: Alignment.bottomLeft,
                   end: Alignment.bottomRight,
                   colors: [Colors.tealAccent, Colors.greenAccent])),
           child: TextButton(
             onPressed: () => onClick(),
-            child: Text(buttonText),
+            child: Text(buttonText, style: const TextStyle(color: Colors.black),),
+          ),
+        ));
+  }
+
+  static Widget commonIconButton(
+      {required Function onClick,
+      required double buttonWidth,
+      required String buttonText,
+      required IconData icon,
+      required double width}) {
+    return SizedBox(
+        width: buttonWidth,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0),
+              gradient: const LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.tealAccent, Colors.greenAccent])),
+          child: TextButton(
+            onPressed: () => onClick(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FaIcon(icon, color: Colors.black54,),
+                SizedBox(
+                  width: width * 0.01,
+                ),
+                Text(
+                  buttonText,
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ],
+            ),
           ),
         ));
   }
